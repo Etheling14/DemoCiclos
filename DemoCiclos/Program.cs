68c4737b6f92538428c59c1c5f4f1d093190e4af
Urrea <Surrea1411@gmail.com>
2023-08-19 17:05:46 -0500

    Double

---
+++ DemoCiclos/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Numbers");
+int i  = 0;
+Console.Write("n? ");
+int n = (int)Convert.ToInt32(Console.ReadLine());
+long sum = 0;
+while (i < n)
+{
+    i++;
+    sum += i;
+    Console.Write($"{i}\t");
+}
+Console.WriteLine($"\nThe sum is....: {sum:N0}");
+double average = (double)sum / (double)n;
+Console.WriteLine($"The average is: {average:N2}");
